f30dc1728cdb030546380f4b955c256e9e1e29b7
Ark.Chang <ark88654@gmail.com>
2022-12-23 21:58:57 +0800

    Format

--- lesson_3/robot-gallery/src/App.tsx
+++ lesson_3/robot-gallery/src/App.tsx
@@ -1,45 +1,45 @@
-import React from "react"
+import React from "react";
 // 因為在react-app.d.ts已經定義好了，故不需要在custom.d.ts重複定義
-import logo from "./assets/images/logo.svg"
+import logo from "./assets/images/logo.svg";
 import styles from "./App.module.css";
-import robots from "./mockdata/robots.json"
-import Robot from "./components/Robot"
-import ShopingCart from "./components/ShoppingCart"
+import robots from "./mockdata/robots.json";
+import Robot from "./components/Robot";
+import ShopingCart from "./components/ShoppingCart";
 
-const html = "<img onerror='alert(\"Hacked!\")' src='invalid-image'/>"
-const jsHacked = "javascript: alert('Hacked!');"
+const html = "<img onerror='alert(\"Hacked!\")' src='invalid-image'/>";
+const jsHacked = "javascript: alert('Hacked!');";
 
 // React組件的回傳值是JSX.Element，並透過ReactDOM的render去呈現畫面（在index.tsx）
 function App() {
   return (
     <>
-    {/* 加入CSS樣式 */}
-    <div className={styles.app}>
-      <div className={styles.appHeader}>
-        <img src={logo} className={styles.appLogo} alt="logo"/>
-        <h1>機器人拍賣online測試網站字數不足我再多補幾個字</h1>
+      {/* 加入CSS樣式 */}
+      <div className={styles.app}>
+        <div className={styles.appHeader}>
+          <img src={logo} className={styles.appLogo} alt="logo" />
+          <h1>機器人拍賣online測試網站字數不足我再多補幾個字</h1>
+        </div>
+        <ShopingCart />
+        <div className={styles.robotList}>
+          {robots.map((r) => (
+            // 若參數屬性不同就會報錯
+            <Robot id={r.id} email={r.email} name={r.name} />
+          ))}
+        </div>
       </div>
-      <ShopingCart/>
-    <div className={styles.robotList}>
-      {robots.map((r) => (
-        // 若參數屬性不同就會報錯
-        <Robot id={r.id} email={r.email} name={r.name} />
-      ))}
-    </div>
-    </div>
 
-    {/* JSX若使用{}來使用變數，可以避免XSS攻擊，React會自動轉譯成文字顯示 */}
-    <div>{html}</div>
-    {/* 但是若駭客使用函式攻擊，則無法避免XSS攻擊 */}
-    <a href={jsHacked}>My websit</a>
-    <ul>
-      {robots.map((r) => (
-        // 若參數屬性不同就會報錯
-        <Robot id={r.id} email={r.email} name={r.name} />
-      ))}
-    </ul>
+      {/* JSX若使用{}來使用變數，可以避免XSS攻擊，React會自動轉譯成文字顯示 */}
+      <div>{html}</div>
+      {/* 但是若駭客使用函式攻擊，則無法避免XSS攻擊 */}
+      <a href={jsHacked}>My websit</a>
+      <ul>
+        {robots.map((r) => (
+          // 若參數屬性不同就會報錯
+          <Robot id={r.id} email={r.email} name={r.name} />
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- lesson_3/robot-gallery/src/components/Robot.tsx
+++ lesson_3/robot-gallery/src/components/Robot.tsx
@@ -1,27 +1,27 @@
-import React from "react"
-import styles from "./Robot.module.css"
+import React from "react";
+import styles from "./Robot.module.css";
 
 // 定義傳入參數的格式類型
 interface RobotProps {
-  id: number
-  name: string
-  email: string
+  id: number;
+  name: string;
+  email: string;
 }
 
 // FC是functional component 函數式組件
 // props是React.ReactNode型態
 const Robot: React.FC<RobotProps> = (props) => {
-  const id = props.id
-  const name = props.name
-  const email = props.email
+  const id = props.id;
+  const name = props.name;
+  const email = props.email;
   return (
     <li>
       <img alt="robot" src={`https://robohash.org/${id}`} />
       <h2>{name}</h2>
       <p>{email}</p>
     </li>
-  )
-}
+  );
+};
 
 const Robot_ES6: React.FC<RobotProps> = ({ id, name, email }) => {
   return (
@@ -31,7 +31,7 @@ const Robot_ES6: React.FC<RobotProps> = ({ id, name, email }) => {
       <h2>{name}</h2>
       <p>{email}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Robot_ES6
+export default Robot_ES6;

1823295d178df70b2e4381e624a8ef3a85224710
Ark.Chang <ark88654@gmail.com>
2022-12-24 13:49:06 +0800

    將Function App改成Class App，並且將資料來源改由讀取API Json資料

--- lesson_3/robot-gallery/src/App.tsx
+++ lesson_3/robot-gallery/src/App.tsx
@@ -9,37 +9,57 @@ import ShopingCart from "./components/ShoppingCart";
 const html = "<img onerror='alert(\"Hacked!\")' src='invalid-image'/>";
 const jsHacked = "javascript: alert('Hacked!');";
 
+interface Props {}
+interface State {
+  robotGallery: any;
+}
+
 // React組件的回傳值是JSX.Element，並透過ReactDOM的render去呈現畫面（在index.tsx）
-function App() {
-  return (
-    <>
-      {/* 加入CSS樣式 */}
-      <div className={styles.app}>
-        <div className={styles.appHeader}>
-          <img src={logo} className={styles.appLogo} alt="logo" />
-          <h1>機器人拍賣online測試網站字數不足我再多補幾個字</h1>
+class App extends React.Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      robotGallery: [],
+    };
+  }
+
+  componentDidMount(): void {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => this.setState({ robotGallery: data }));
+  }
+
+  render() {
+    return (
+      <>
+        {/* 加入CSS樣式 */}
+        <div className={styles.app}>
+          <div className={styles.appHeader}>
+            <img src={logo} className={styles.appLogo} alt="logo" />
+            <h1>機器人拍賣online測試網站字數不足我再多補幾個字</h1>
+          </div>
+          <ShopingCart />
+          <div className={styles.robotList}>
+            {this.state.robotGallery.map((r) => (
+              // 若參數屬性不同就會報錯
+              <Robot id={r.id} email={r.email} name={r.name} />
+            ))}
+          </div>
         </div>
-        <ShopingCart />
-        <div className={styles.robotList}>
+
+        {/* JSX若使用{}來使用變數，可以避免XSS攻擊，React會自動轉譯成文字顯示 */}
+        <div>{html}</div>
+        {/* 但是若駭客使用函式攻擊，則無法避免XSS攻擊 */}
+        <a href={jsHacked}>My websit</a>
+        <ul>
           {robots.map((r) => (
             // 若參數屬性不同就會報錯
             <Robot id={r.id} email={r.email} name={r.name} />
           ))}
-        </div>
-      </div>
-
-      {/* JSX若使用{}來使用變數，可以避免XSS攻擊，React會自動轉譯成文字顯示 */}
-      <div>{html}</div>
-      {/* 但是若駭客使用函式攻擊，則無法避免XSS攻擊 */}
-      <a href={jsHacked}>My websit</a>
-      <ul>
-        {robots.map((r) => (
-          // 若參數屬性不同就會報錯
-          <Robot id={r.id} email={r.email} name={r.name} />
-        ))}
-      </ul>
-    </>
-  );
+        </ul>
+      </>
+    );
+  }
 }
 
 export default App;

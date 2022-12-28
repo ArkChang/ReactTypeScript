ead0d7b5516fce97289fa4538e1d93beb86bb15e
Ark.Chang <ark88654@gmail.com>
2022-12-28 22:49:06 +0800

    新增async/await 非同步模式語法

--- lesson_4/robot-gallery/src/App.tsx
+++ lesson_4/robot-gallery/src/App.tsx
@@ -20,9 +20,14 @@ const App: React.FC = (props) => {
   }, [count]);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => setRobotGallery(data));
+    const fetchData = async () => {
+      const responses = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const data = await responses.json();
+      setRobotGallery(data);
+    };
+    fetchData();
   }, []);
 
   return (
